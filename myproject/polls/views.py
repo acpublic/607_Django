2d91d1021e00e708b2f457a909777d5572fffd58
acpublic <168211689+acpublic@users.noreply.github.com>
2024-09-03 00:36:44 +0900

    Update views.py

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from datetime import datetime
+import os
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def get(request):
-    return render(request, 'polls/index.html')
+    data = {
+        'data1': datetime.now(),
+        'data2': os.path.abspath(__file__)
+    }
+    return render(request, 'polls/index.html', data)
